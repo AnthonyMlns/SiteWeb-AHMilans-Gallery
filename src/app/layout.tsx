752c2e928395ae984ea8d5377b75ce7e1f6578d2
AnthonyMlns <milans.antony@gmail.com>
2026-06-23 15:06:45 +0200

    fix: switch to local fonts + webpack for Windows compat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,24 @@
 import type { Metadata } from 'next'
-import { Crimson_Text, Geist } from 'next/font/google'
+import localFont from 'next/font/local'
 import Providers from '@/components/layout/Providers'
 import { SITE_NAME, SITE_URL, SITE_DESCRIPTION } from '@/lib/config'
 import './globals.css'
 
-const crimson = Crimson_Text({
-  weight: ['400', '600', '700'],
-  subsets: ['latin'],
+const crimson = localFont({
+  src: [
+    { path: '../fonts/CrimsonText-Regular.ttf', weight: '400', style: 'normal' },
+    { path: '../fonts/CrimsonText-Italic.ttf', weight: '400', style: 'italic' },
+    { path: '../fonts/CrimsonText-SemiBold.ttf', weight: '600', style: 'normal' },
+    { path: '../fonts/CrimsonText-SemiBoldItalic.ttf', weight: '600', style: 'italic' },
+    { path: '../fonts/CrimsonText-Bold.ttf', weight: '700', style: 'normal' },
+    { path: '../fonts/CrimsonText-BoldItalic.ttf', weight: '700', style: 'italic' },
+  ],
   display: 'swap',
   variable: '--font-crimson',
 })
 
-const geist = Geist({
-  subsets: ['latin'],
+const geist = localFont({
+  src: '../fonts/Geist-Variable.ttf',
   display: 'swap',
   variable: '--font-geist',
 })
